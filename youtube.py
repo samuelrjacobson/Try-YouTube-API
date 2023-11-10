bfd179d864edb4d8b1d1c7c4ae69580988e7a482
samuelrjacobson <132033453+samuelrjacobson@users.noreply.github.com>
2023-11-10 23:00:30 +0000

    fix: Add search function to youtube.py
    Added searching function so user may search using query term and max results
    (number of results they want shown per page), and added requirements.txt
    
    issue #3

--- youtube.py
+++ youtube.py
@@ -1,3 +1,29 @@
+import sys
 import config
+from googleapiclient.discovery import build
+from googleapiclient.errors import HttpError
 
-print(config.API_KEY)
+DEVELOPER_KEY = config.API_KEY
+YOUTUBE_API_SERVICE_NAME = 'youtube'
+YOUTUBE_API_VERSION = 'v3'
+
+def youtube_search(query_term, max_results):
+    youtube = build(YOUTUBE_API_SERVICE_NAME, YOUTUBE_API_VERSION,
+        developerKey=DEVELOPER_KEY)
+
+    #Call the search.List method to retrieve results matching the specified query term.
+    search_response = youtube.search().list(
+        q=query_term,
+        part='id,snippet',
+        maxResults=max_results,
+    ).execute()
+
+    return search_response
+
+if __name__ == "__main__":
+    query_term = sys.argv[1]
+    max_results = sys.argv[2]
+    try:
+        print(youtube_search(query_term, max_results))
+    except HttpError as e:
+        print('An HTTP error %d occurred: \n%s' % (type(e).__name__, str(e)))
