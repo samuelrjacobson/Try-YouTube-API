2c53017afedb33d4f23b883f374053eb156afb95
samuelrjacobson <132033453+samuelrjacobson@users.noreply.github.com>
2023-11-10 23:29:38 +0000

    fix: Add second page to displayed results
    Changed youtube.py to print out first two pages instead of just the first.
    youtube_search function now prints the results and returns the nextPageToken,
    instead of returning the search results to be printed by caller.
    youtube_search function is called twice, takes nextPageToken as parameter, and
    determines which page to print based on nextPageToken.
    
    issue #7

--- youtube.py
+++ youtube.py
@@ -7,24 +7,35 @@ DEVELOPER_KEY = config.API_KEY
 YOUTUBE_API_SERVICE_NAME = 'youtube'
 YOUTUBE_API_VERSION = 'v3'
 
-def youtube_search(query_term, max_results):
+def youtube_search(query_term, max_results, npt):
     youtube = build(YOUTUBE_API_SERVICE_NAME, YOUTUBE_API_VERSION,
         developerKey=DEVELOPER_KEY)
 
-    #Call the search.List method to retrieve results matching the specified query term.
-    search_response = youtube.search().list(
-        q=query_term,
-        part='id,snippet',
-        maxResults=max_results,
-    ).execute()
+    # Call the search.List method to retrieve results matching the specified query term.
+    if(npt == None):
+        search_response = youtube.search().list(
+            q = query_term,
+            part = 'id,snippet',
+            maxResults = max_results,
+        ).execute()
+    else:
+        search_response = youtube.search().list(
+            q = query_term,
+            part = 'id,snippet',
+            maxResults = max_results,
+            pageToken = npt,
+        ).execute()
 
-    video = search_response['items']
-    return video
+    print(search_response["items"])
+    print("\t")
+    
+    return search_response["nextPageToken"]
 
 if __name__ == "__main__":
     query_term = sys.argv[1]
     max_results = sys.argv[2]
     try:
-        print(youtube_search(query_term, max_results))
+        npt = youtube_search(query_term, max_results, None)
+        youtube_search(query_term, max_results, npt)
     except HttpError as e:
         print('An HTTP error %d occurred: \n%s' % (type(e).__name__, str(e)))
